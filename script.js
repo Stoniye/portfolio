d003729257183225262d023ef0e8434799eba34a
Stonie <aronstein10@gmail.com>
2025-08-02 16:42:00 +0200

    Fixed date calculator, new Voxel Game image

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ function onLoad(){
 //Load year numbers in Text
 function loadNumbers() {
     document.getElementById("yearsOfExperience").innerHTML = getYearsPassed(new Date(2020, 7));
-    document.getElementById("age").innerHTML = getYearsPassed(new Date(2006, 7, 14));
-    document.getElementById("descriptionText").innerHTML = document.getElementById("descriptionText").innerHTML.replace("#age#", getYearsPassed(new Date(2006, 7, 14))).replace("#exp#", getYearsPassed(new Date(2020, 7)));
+    document.getElementById("age").innerHTML = getYearsPassed(new Date(2006, 7));
+    document.getElementById("descriptionText").innerHTML = document.getElementById("descriptionText").innerHTML.replace("#age#", getYearsPassed(new Date(2006, 7))).replace("#exp#", getYearsPassed(new Date(2020, 7)));
 }
 
 function getYearsPassed(date) {
-    const currentDate = new Date();
-    let yearsPassed = currentDate.getFullYear() - date.getFullYear();
+    const today = new Date();
 
-    if (currentDate.getMonth() < date.getMonth() || (currentDate.getMonth() === date.getMonth() && currentDate.getDate() < date.getDate()))
-        yearsPassed--;
+    let years = today.getFullYear() - date.getFullYear();
+    const monthDiff = today.getMonth() - date.getMonth();
 
-    return `${yearsPassed}`;
+    if (monthDiff < 0)
+        years--;
+
+    return years;
 }
 
 //Tools scroll animation
