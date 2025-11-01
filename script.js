c08c798f01575bd889a97c4288702bfc2934a516
Stonie <aronstein10@gmail.com>
2025-11-01 19:21:20 +0100

    feat: basic development of the new layout

--- script.js
+++ script.js
@@ -9,12 +9,10 @@ function onLoad(){
 
 //Load year numbers in Text
 function loadNumbers() {
-    age = getYearsPassed(new Date(2006, 7));
+    age = getYearsPassed(new Date(2006, 7, 14));
     experience = getYearsPassed(new Date(2020, 7));
 
-    document.getElementById("yearsOfExperience").innerHTML = experience;
-    document.getElementById("age").innerHTML = age;
-    document.getElementById("descriptionText").innerHTML = document.getElementById("descriptionText").innerHTML.replace("#age#", age).replace("#exp#", experience);
+    document.getElementById("textWithYears").innerHTML = document.getElementById("textWithYears").innerHTML.replace("#age#", age).replace("#exp#", experience);
 }
 
 function getYearsPassed(date) {
